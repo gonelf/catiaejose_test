e41d83e4109a2768c2591847d5549f8d87edf864
Gonçalo Henriques <gonelf@gmail.com>
2021-05-15 20:04:12 +0100

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -2,7 +2,7 @@ function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires="+ d.toUTCString();
-  var c = cname + "=" + encodeURIComponent(cvalue) + ";" + expires + ";path=/";
+  var c = encodeURIComponent(cname + "=" + cvalue + ";" + expires + ";path=/");
   console.log(c);
   document.cookie = c;
 }
